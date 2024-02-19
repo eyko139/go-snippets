3d42c53ee53672759bd2c72e6185f914a5955092
Lukas Kühne <mud2@gmx.de>
2024-02-19 21:28:16 +0100

    error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,7 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"slices"
 	"strconv"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/julienschmidt/httprouter"
 
 	"github.com/eyko139/go-snippets/config"
 	"github.com/eyko139/go-snippets/internal/models"
@@ -26,7 +31,7 @@ func home(cfg *config.Config) http.HandlerFunc {
 			cfg.Hlp.ServerError(w, err)
 			return
 		}
-		data := cfg.Hlp.NewTemplateData()
+		data := cfg.Hlp.NewTemplateData(r)
 		data.Snippets = latest
 
 		cfg.Hlp.Render(w, http.StatusOK, "home.html", data)
@@ -37,10 +42,18 @@ func home(cfg *config.Config) http.HandlerFunc {
 	}
 }
 
+func snippetCreate(cfg *config.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data := cfg.Hlp.NewTemplateData(r)
+		cfg.Hlp.Render(w, http.StatusOK, "create.html", data)
+	}
+}
+
 func snippetView(cfg *config.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		id, err := strconv.Atoi(r.URL.Query().Get("id"))
-		if err != nil {
+		params := httprouter.ParamsFromContext(r.Context())
+		id, err := strconv.Atoi(params.ByName("id"))
+		if err != nil || id < 1 {
 			cfg.Hlp.NotFound(w)
 		}
 		snippet, err := cfg.Snippets.Get(id)
@@ -56,7 +69,7 @@ func snippetView(cfg *config.Config) http.HandlerFunc {
 			cfg.Hlp.ServerError(w, err)
 			return
 		}
-		data := cfg.Hlp.NewTemplateData()
+		data := cfg.Hlp.NewTemplateData(r)
 		data.Snippet = snippet
 
 		cfg.Hlp.Render(w, http.StatusOK, "view.html", data)
@@ -67,7 +80,7 @@ func snippetView(cfg *config.Config) http.HandlerFunc {
 	}
 }
 
-func snippetCreate(cfg *config.Config) http.HandlerFunc {
+func snippetCreatePost(cfg *config.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			// headers are automatically converted into correct case according to http standard
@@ -75,15 +88,40 @@ func snippetCreate(cfg *config.Config) http.HandlerFunc {
 			cfg.Hlp.ClientError(w, http.StatusMethodNotAllowed)
 			return
 		}
-		title := "O snail"
-		content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n Kobayashi Issa"
-		expires := 7
+		err := r.ParseForm()
+		if err != nil {
+			cfg.Hlp.ClientError(w, http.StatusBadRequest)
+			return
+		}
+		title := r.PostForm.Get("title")
+		content := r.PostForm.Get("content")
+		expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+		if err != nil {
+			cfg.Hlp.ClientError(w, http.StatusBadRequest)
+			return
+		}
+		//  Validate user input
+		fieldErrors := make(map[string]string)
+		if strings.TrimSpace(title) == "" {
+			fieldErrors["title"] = "This field cannot be blank"
+		} else if utf8.RuneCountInString(title) > 100 {
+			fieldErrors["title"] = "Field cannot be longer than 100 characters"
+		}
+
+		if strings.TrimSpace(content) == "" {
+			fieldErrors["content"] = "Content cannot be empty"
+		}
+
+		expirationDates := []int{1, 7, 365}
+		if !slices.Contains(expirationDates, expires) {
+			fieldErrors["expires"] = fmt.Sprintf("Expiration date may only contain one of the following values %d", expires)
+		}
+
 		id, err := cfg.Snippets.Insert(title, content, expires)
 		if err != nil {
 			cfg.Hlp.ServerError(w, err)
 		}
-		http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id),
+		http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id),
 			http.StatusSeeOther)
-		w.Write([]byte("Creating new snippet"))
 	}
 }
