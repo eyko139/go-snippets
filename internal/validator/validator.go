efea0d6897b990c83d2761b70212d6e84df87069
Lukas Kühne <lukas.kuehne@prodyna.com>
2024-02-28 12:07:21 +0100

    Add form validation with deticated validator module

---
+++ internal/validator/validator.go
@@ -0,0 +1,52 @@
+package validator
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+type Validator struct {
+	FieldErrors map[string]string
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0
+}
+
+func (v *Validator) AddFieldError(field string, message string) {
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+	v.FieldErrors[field] = message
+	if _, exists := v.FieldErrors[field]; !exists {
+		v.FieldErrors[field] = message
+	}
+}
+
+// CheckField() adds an error message to the FieldErrors map only if a
+// validation check is not 'ok'.
+func (v *Validator) CheckField(condition bool, key, message string) {
+	if !condition {
+		v.AddFieldError(key, message)
+	}
+}
+
+// NotBlank() returns true if a value is not an empty string.
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+// MaxChars() returns true if a value contains no more than n characters.
+func MaxChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) <= n
+}
+
+// PermittedInt() returns true if a value is in a list of permitted integers.
+func PermittedInt(value int, permittedValues ...int) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
+			return true
+		}
+	}
+	return false
+}
